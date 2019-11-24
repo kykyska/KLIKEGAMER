c5884030f3146dd80e4a47fee2aa40bfb67ec8ea
kykyska <58131686+kykyska@users.noreply.github.com>
2019-11-24 11:29:25 +0300

    Add files via upload

---
+++ ПРОБА.py
@@ -0,0 +1,8 @@
+import mcpi.minecraft as minecraft
+
+import time
+
+mc = minecraft.Minecraft.create
+
+while True:
+    mc.setBlocks(368,75,314,368,75,314,14)
---
+++ ну ок.py
@@ -0,0 +1,22 @@
+import mcpi.minecraft as minecraft
+
+import time
+
+mc = minecraft.Minecraft.create()
+
+mc.setBlocks(315,63,253,315,70,240,24)
+mc.setBlock(315,71,253,346,66,253,24)
+mc.setBlocks(346,71,253,346,67,233,24)
+mc.setBlocks(346,71,233,315,65,236,24)
+mc.setBlocks(316,64,252,345,71,234,0)
+mc.setBlocks(317,65,233,320,67,233,0)
+mc.setBlocks(345,66,253,342,68,254,0)
+mc.setBlock(318,66,232,54)
+mc.setBlocks(341,66,252,341,68,247,24)
+mc.setBlocks(345,65,252,316,65,234,24)
+mc.setBlocks(346,66,237,346,65,247,24)
+mc.setBlocks(315,71,234,315,71,253,24)
+mc.setBlocks(340,68,247,337,66,247,24)
+mc.setBlocks(345,68,242,337,66,242,24)
+mc.setBlocks(336,68,247,333,66,247,24)
+mc.setBlocks(333,66,242,337,68,242,24)
